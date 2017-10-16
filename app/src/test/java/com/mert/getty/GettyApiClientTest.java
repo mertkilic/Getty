397330b7fc05b10f079efba1cd299255ca45d7d2
Mert Kilic <mert.kilic@icterra.com>
2017-10-16 11:58:34 +0300

    Unit tests are updated through rx java

--- app/src/test/java/com/mert/getty/GettyApiClientTest.java
+++ app/src/test/java/com/mert/getty/GettyApiClientTest.java
@@ -2,6 +2,7 @@ package com.mert.getty;
 
 import com.mert.getty.data.GettyService;
 import com.mert.getty.data.api.GettyApiInterceptor;
+import com.mert.getty.data.api.GettyClientConfig;
 import com.mert.getty.data.model.GettyResponse;
 import com.mert.getty.data.model.Image;
 
@@ -12,8 +13,13 @@ import org.junit.runners.JUnit4;
 
 import java.io.IOException;
 
+import io.reactivex.annotations.NonNull;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
 import okhttp3.OkHttpClient;
+import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 import static org.hamcrest.core.Is.is;
@@ -38,6 +44,7 @@ public class GettyApiClientTest extends MockWebServerTest {
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(endpoint)
                 .client(new OkHttpClient.Builder().addInterceptor(new GettyApiInterceptor()).build())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
 
         service = retrofit.create(GettyService.class);
@@ -46,29 +53,78 @@ public class GettyApiClientTest extends MockWebServerTest {
     @Test
     public void checkApiKey() throws IOException, InterruptedException {
         enqueueResponse("search-response.json");
-        service.search("hair", 1, 1).execute();
-        assertRequestContainsHeader("Api-Key", BuildConfig.GETTY_API_KEY);
+        service.search("hair", 1, 1)
+                .subscribeOn(Schedulers.io())
+                .subscribe(new DisposableObserver<Response<GettyResponse>>() {
+                    @Override
+                    public void onNext(@NonNull Response<GettyResponse> response) {
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+        assertRequestContainsHeader("Api-Key", GettyClientConfig.getApiKey());
     }
 
     @Test
     public void checkRequestSentToCorrectEndPoint() throws IOException, InterruptedException {
         enqueueResponse(200);
-        service.search("hair", 1, 1).execute();
+        service.search("hair", 1, 1).subscribeOn(Schedulers.io())
+                .subscribe(new DisposableObserver<Response<GettyResponse>>() {
+                    @Override
+                    public void onNext(@NonNull Response<GettyResponse> response) {
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
         assertGetRequestSentTo("/search/images?phrase=hair&page_size=1&page=1");
     }
 
     @Test
     public void search() throws IOException, InterruptedException {
         enqueueResponse("search-response.json");
-        GettyResponse response = service.search("hair", 1, 1).execute().body();
-        assertNotNull(response);
-        assertThat(response.getResultCount(), is(1));
-        Image image = response.getImages().get(0);
-        assertNotNull(image);
-        assertEquals(image.getUrl(), "https://media.gettyimages.com/photos/afgan-hound-with-hair-blowing-studio-shot-picture-idsb10069853n-001?b=1&k=6&m=sb10069853n-001&s=170x170&h=FpW3R52LBELROoHmBQAROq6c0XUbJLQc6qTESAfWtt0=");
-        assertNull(image.getCaption());
-        assertEquals(image.getTitle(), "Afgan Hound with hair blowing, studio shot");
-        assertEquals(image.getId(), "sb10069853n-001");
+        service.search("hair", 1, 1)
+                .subscribeOn(Schedulers.io())
+                .subscribe(new DisposableObserver<Response<GettyResponse>>() {
+                    @Override
+                    public void onNext(@NonNull Response<GettyResponse> response) {
+                        GettyResponse gettyResponse = response.body();
+                        assertNotNull(gettyResponse);
+                        assertThat(gettyResponse.getResultCount(), is(1));
+                        Image image = gettyResponse.getImages().get(0);
+                        assertNotNull(image);
+                        assertEquals(image.getUrl(), "https://media.gettyimages.com/photos/afgan-hound-with-hair-blowing-studio-shot-picture-idsb10069853n-001?b=1&k=6&m=sb10069853n-001&s=170x170&h=FpW3R52LBELROoHmBQAROq6c0XUbJLQc6qTESAfWtt0=");
+                        assertNull(image.getCaption());
+                        assertEquals(image.getTitle(), "Afgan Hound with hair blowing, studio shot");
+                        assertEquals(image.getId(), "sb10069853n-001");
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
     }
 
 }
